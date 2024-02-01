404eb8cb67dde8be9f652f339a55ee66e36d96f0
AntoineGillesLordet <antoine.gilleslordet@gmail.com>
2024-02-01 07:49:01 -0800

    added ztf logs reading and better bgs handling

--- code/load.py
+++ code/load.py
@@ -5,11 +5,16 @@ import fitsio
 from glob import glob
 
 
-def load_bgs(path=None, filename='Uchuu.csv', columns=['RA', 'DEC', 'Z', 'Z_COSMO', 'STATUS']):
+def load_bgs(path=None, filename='Uchuu.csv', columns=['RA', 'DEC', 'Z', 'Z_COSMO', 'STATUS'], in_desi=True):
     try:
         filepath = glob('../../**/' + filename, recursive=True)[0]
         df = pandas.read_csv(filepath, index_col=0)
-        print(f'Found file {filepath} with columns {df.columns}')
+        if set(map(lambda s: s.lower(), columns)).issubset(set(df.columns)):
+            print(f'Found file {filepath} with columns {df.columns}')
+            return df
+        else:
+            print(f'Found file {filepath} with columns {df.columns} but columns {columns} were prompted, defaulting to fits file')
+            raise IndexError
     except IndexError:
 
         if path is None:
@@ -18,15 +23,21 @@ def load_bgs(path=None, filename='Uchuu.csv', columns=['RA', 'DEC', 'Z', 'Z_COSM
         data = fitsio.read(path, columns=columns)
 
         df = pandas.DataFrame(data)
-        df.rename(columns={'RA':'ra', 'DEC':'dec','Z':'z', 'Z_COSMO':'z_cosmo'}, inplace=True)
+        df.rename(columns=lambda s: s.lower(), inplace=True)
         for col in df.columns:
             if (df[col].dtype == np.dtype('>f8') or df[col].dtype == np.dtype('>f4')):
                 df[col] = np.float64(df[col])
             elif df[col].dtype == np.dtype('>i4'):
                 df[col] = np.int64(df[col])
 
-        if 'STATUS' in df.columns:
-            df['in_desi'] = df['STATUS'] & 2**1 != 0
-            df.drop(columns=['STATUS'], inplace=True)
+        if in_desi and 'status' in df.columns:
+            df['in_desi'] = df['status'] & 2**1 != 0
+            df.drop(columns=['status'], inplace=True)
 
-    return df
+    return df[df['in_desi']]
+
+def extract_ztf(start_time=58179, end_time=59215):
+    with open('data/ztf_survey.pkl','rb') as file:
+        survey = pickle.load(file)
+    survey.set_data(survey.data[(survey.data['mjd'] > start_time) & (survey.data['mjd'] < end_time)])
+    return survey
