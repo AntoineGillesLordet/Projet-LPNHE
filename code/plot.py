8fe3ddf2b492bc4ca43d74e5610e2e4ffcdcde9c
AntoineGillesLordet <antoine.gilleslordet@gmail.com>
2024-02-07 07:58:00 -0800

    scatter stackable on figure, corner kwargs

--- code/plot.py
+++ code/plot.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import corner
 
-def corner_(data, var_names=None, labels=None, title=None):
-    fig = corner.corner(
-        data,
-        var_names=var_names,
+def corner_(data, var_names=None, fig=None, labels=None, title=None, **kwargs):
+    params = dict(var_names=var_names,
         show_titles=True,
         bins=50,
         smooth=0.9,
@@ -17,9 +15,16 @@ def corner_(data, var_names=None, labels=None, title=None):
         plot_datapoints=True,
         fill_contours=True,
         max_n_ticks=7,
-        hist_kwargs=dict(density=True, color="b"),
+        hist_kwargs=dict(density=True, color=kwargs.get("color",'b')),
         labels=labels,
         alpha=0.2,
+        fig = fig)
+    
+    params.update(kwargs)
+
+    fig = corner.corner(
+        data,
+        **params,
     )
 
     if title:
@@ -28,14 +33,16 @@ def corner_(data, var_names=None, labels=None, title=None):
     fig.set_dpi(50)
     return fig
 
-def mollweide_scatter(data):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="mollweide")
+def mollweide_scatter(data, ax=None, **kwargs):
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="mollweide")
+
+    params=dict(s=1, color='k',alpha=0.3, marker=".")
+    params.update(kwargs)
     ax.scatter(
         (data["ra"] - 360 * (data["ra"] > 180)) * np.pi / 180,
         data["dec"] * np.pi / 180,
         linestyle="",
-        marker=".",
-        s=1,
+        **params,
     )
-    plt.show()
