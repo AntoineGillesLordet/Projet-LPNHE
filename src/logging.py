047c8b0f64e969d55b6feeda59cb43ad2a53342a
AntoineGillesLordet <antoine.gilleslordet@gmail.com>
2024-06-04 02:00:30 -0700

    better logging

--- src/logging.py
+++ src/logging.py
@@ -1,21 +1,26 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(
-    logging.Formatter("%(asctime)s %(levelname)-8s: %(message)s", datefmt="%H:%M")
-)
+logger = logging.getLogger("ZTFxUchuu")
 
-stream_handler.setLevel(logging.INFO)
-logger.addHandler(stream_handler)
+def setup_logger(filename='logs.txt'):
 
-file_handler = logging.FileHandler("logs.txt", mode="a")
-file_handler.setFormatter(
-    logging.Formatter(
-        "%(asctime)s %(levelname)-8s: %(message)s", datefmt="%Y/%m/%d %H:%M"
+    logger = logging.getLogger("ZTFxUchuu")
+    
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(
+        logging.Formatter("%(asctime)s %(levelname)-8s: %(message)s", datefmt="%H:%M")
     )
-)
 
-file_handler.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
+    stream_handler.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler(filename, mode="a")
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)-8s: %(message)s", datefmt="%Y/%m/%d %H:%M"
+        )
+    )
+
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
