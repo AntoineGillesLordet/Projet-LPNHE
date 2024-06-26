a8cac0de0ab1260eec9a8e315d4a5f381314b5c4
AntoineGillesLordet <antoine.gilleslordet@gmail.com>
2024-06-26 05:21:06 -0700

    Added date range and zmax in SN model

--- src/sample_Uchuu.py
+++ src/sample_Uchuu.py
@@ -1,12 +1,12 @@
 from skysurvey.target.snia import SNeIaMagnitude, SNeIaColor, SNeIaStretch
 from skysurvey.target import Transient
-from skysurvey.effects import dust
+from skysurvey.effects import milkyway
 import sncosmo
 import numpy as np
 from .load import load_bgs
 
 
-def rand_positions(positions, size=1, weights=None, zcut=0.1):
+def rand_positions(positions, size=1, weights=None, zcut=0.06):
     positions_ = positions[positions["z"] < zcut]
     index = np.random.choice(positions_.index, size=size, p=weights)
 
@@ -24,14 +24,14 @@ class SNeIa_full_bgs(Transient):
     _KIND = "SNIa"
     _RATE = 2.35 * 10**4  # /yr/Gpc^3 Perley 2020
 
-    def __init__(self, path=None, filename="Uchuu.csv"):
+    def __init__(self, path=None, filename="Uchuu.csv", date_range=[58179, 59215], zmax=0.06):
         super().__init__()
         # {'model': func, 'prop': dict, 'input':, 'as':}
         self.set_model(dict(
             redshift={"kwargs": {"zmax": 0.2}},
             x1={"func": SNeIaStretch.nicolas2021},
             c={"func": SNeIaColor.intrinsic_and_dust},
-            t0={"func": np.random.uniform, "kwargs": {"low": 58179, "high": 59215}},
+            t0={"func": np.random.uniform, "kwargs": {"low": date_range[0], "high": date_range[1]}},
             magabs={
                 "func": SNeIaMagnitude.tripp1998,
                 "kwargs": {"x1": "@x1", "c": "@c", "mabs": -19.3, "sigmaint": 0.10},
@@ -46,10 +46,10 @@ class SNeIa_full_bgs(Transient):
             },  # because it needs to call sncosmo_model.get(param_name)
             radecz={
                 "func": rand_positions,
-                "kwargs": {"positions": load_bgs(path=path, filename=filename), "zcut": 0.06},
+                "kwargs": {"positions": load_bgs(path=path, filename=filename), 'zcut': zmax},
                 "as": ["ra", "dec", "z", "bgs_id", "z_cosmo"],
             },
-            mwebv={"func": dust.get_mwebv, "kwargs": {"ra": "@ra", "dec": "@dec"}},
+            mwebv={"func": milkyway.get_mwebv, "kwargs": {"ra": "@ra", "dec": "@dec"}},
         ))
         
         source = sncosmo.SALT2Source(modeldir='./data/SALT_snf', m0file='nacl_m0_test.dat', m1file='nacl_m1_test.dat', clfile='nacl_color_law_test.dat')
