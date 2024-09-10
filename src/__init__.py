afae5fd6c05b5cb2b9438d8b6f364690ef7425a0
AntoineGillesLordet <antoine.gilleslordet@gmail.com>
2024-09-10 07:20:29 -0700

    Fixed Planck15

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,13 @@ import jax
 jax.config.update("jax_debug_nans", True)
 jax.config.update("jax_enable_x64", True)
 
+from astropy.cosmology import Planck15, FlatLambdaCDM
+Planck15 = FlatLambdaCDM(name='Planck15', H0=Planck15.H0, Om0=0.3089, Tcmb0=Planck15.Tcmb0, Neff=Planck15.Neff, m_nu=Planck15.m_nu, Ob0=Planck15.Ob0)
+
 from .load import *
 from .plot import *
 from .fit import *
 from .sample_Uchuu import *
 from .tools import *
 from .logging import *
+
